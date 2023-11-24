c6e16426688cbedebcc9d031fe5a783b4a812ffe
Lucas Miño <94700933+LucasDMino@users.noreply.github.com>
2023-11-23 21:14:10 -0300

    Update CreateTable.sql

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -1,10 +1,12 @@
 
-DROP TABLE IF EXISTS dbo.Cliente
---Tabla Cliente
-CREATE TABLE dbo.Cliente (
-    idCliente INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    idPersona INT FOREIGN KEY REFERENCES Persona(idPersona) NOT NULL,
-    FechaIngreso DATETIME NOT NULL DEFAULT(getdate()),
-	Calificacion VARCHAR(20) NOT NULL,
-	Estado VARCHAR(20) NULL
+--Tabla Usuario
+DROP TABLE IF EXISTS dbo.Usuario
+CREATE TABLE dbo.Usuario (
+    idUsuario INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	idPersona INT FOREIGN KEY REFERENCES Persona(idPersona) NOT NULL,
+    NombreUsuario VARCHAR(50) NOT NULL,
+    Contrasenha VARCHAR(50) NOT NULL,
+	Nivel VARCHAR(50) NOT NULL,
+	Estado VARCHAR(50) NOT NULL
 );
+
